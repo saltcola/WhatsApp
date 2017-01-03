d0388278646ad9d4e21897eb3be481a190d20853
Xiguang Liu <xiguang.liu@uconn.edu>
2017-01-03 12:05:30 -0500

    basic chat page setup

--- client/imports/app/app.component.ts
+++ client/imports/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { StatusBar } from "ionic-native";
 import template from "./app.component.html";
-import style from "./app.component.scss";
+
+import {TabsContainerComponent} from "../pages/tabs-container/tabs-container.component";
+
 
 @Component({
   selector: "app",
-  template,
-  styles: [ style ]
+  template
 })
 export class AppComponent {
+	rootPage = TabsContainerComponent;
+	
 	constructor(platform: Platform) {
 	    platform.ready().then(() => {
 	      // Okay, so the platform is ready and our plugins are available.
--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -4,15 +4,24 @@ import { AppComponent } from "./app.component";
 //Add IonicModule
 import { IonicApp, IonicModule } from "ionic-angular";
 
+import {TabsContainerComponent} from "../pages/tabs-container/tabs-container.component";
+import {ChatsComponent} from "../pages/chats/chats.component";
+import {MomentModule} from "angular2-moment";
+
+
 
 @NgModule({
   // Components, Pipes, Directive
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsContainerComponent,
+    ChatsComponent
   ],
   // Entry Components
   entryComponents: [
-    AppComponent
+    AppComponent,
+    TabsContainerComponent,
+    ChatsComponent
   ],
   // Providers
   providers: [
@@ -20,7 +29,8 @@ import { IonicApp, IonicModule } from "ionic-angular";
   // Modules
   //We removed BrowserModule since all the declarations and providers are included in IonicModule.
   imports: [
-    IonicModule.forRoot(AppComponent)
+    IonicModule.forRoot(AppComponent),
+    MomentModule
   ],
   // Main Component
   //We also added IonicApp component which is a root component that lives on top of our AppComponent.
